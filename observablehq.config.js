51d97bd25f5d7eecd556b916aba267d43eec8254
Jonathan St-Onge <jonathanstonge7@gmail.com>
2024-08-22 21:07:11 -0400

    more structure

--- observablehq.config.js
+++ observablehq.config.js
@@ -4,15 +4,17 @@ export default {
   title: "MOCS Fall 2024",
   pages: [
     { name: "Getting Started", path: "/getting-started" },
-    { name: "Week 1: Why models", path: "/W1-why-models" },
-    { name: "Week 2: Modeling", path: "/W2-modeling" },
+    { name: "Why models", path: "/W1-why-models" },
+    { name: "Modeling", path: "/W2-modeling" },
     {
       name: "Dynamics",
       open: false,
       pager: "M1-Dynamics",
       pages: [
         { name: "Getting started", path: "M1-Dynamics/getting-started" },
-        { name: "Week 3: Compartments", path: "M1-Dynamics/W3-compartments" },
+        { name: "Discrete models", path: "M1-Dynamics/W3-discrete" },
+        { name: "Continous models", path: "M1-Dynamics/W4-continuous" },
+        { name: "Chaos", path: "M1-Dynamics/W5-chaos" },
       ]
     },
     {
@@ -21,7 +23,8 @@ export default {
       pager: "M2-Structure-part-1",
       pages: [
         { name: "Getting started", path: "M2-Structure-part-1/getting-started" },
-        { name: "A game of life", path: "M2-Structure-part-1/W6-game-of-life" },
+        { name: "A game of life", path: "M2-Structure-part-1/W6-intro-ca" },
+        { name: "Stochastic CA", path: "M2-Structure-part-1/W7-stochastic-ca" },
       ]
     },
     {
@@ -30,6 +33,10 @@ export default {
       pager: "M3-Structure-part-2",
       pages: [
         { name: "Getting started", path: "M3-Structure-part-2/getting-started" },
+        { name: "Networks", path: "M3-Structure-part-2/W9-networks" },
+        { name: "Models on networks", path: "M3-Structure-part-2/W10-on-networks" },
+        { name: "Models of networks", path: "M3-Structure-part-2/W11-of-networks" },
+        { name: "Models on and of networks", path: "M3-Structure-part-2/W12-on-and-of-networks" },
       ]
     },
     {
@@ -38,6 +45,8 @@ export default {
       pager: "M4-Dynamics-and-Structure",
       pages: [
         { name: "Getting started", path: "M4-Dynamics-and-Structure/getting-started" },
+        { name: "ABMs in archeology", path: "M4-Dynamics-and-Structure/W13-crabtree-abms" },
+        { name: "Evolutionary game theory", path: "M4-Dynamics-and-Structure/W14-evo-game-theory" },
       ]
     },
     {
